df36ca82c5b575d2d1f15251be898a2eb85ed2c3
harivansh-afk <rathiharivansh@gmail.com>
2025-10-27 10:25:50 -0400

    transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,7 +7,7 @@ return {
     config = function()
       require("gruvbox").setup({
         contrast = "hard", -- can be "hard", "soft" or empty string
-        transparent_mode = false,
+        transparent_mode = true,
       })
     end,
   },
