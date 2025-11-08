161e0c20e659056fec8050eafb0f559c96e7bfb6
harivansh-afk <rathiharivansh@gmail.com>
2025-11-08 14:31:02 -0500

    update config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,7 +7,7 @@ return {
     config = function()
       require("gruvbox").setup({
         contrast = "hard", -- can be "hard", "soft" or empty string
-        transparent_mode = true,
+        transparent_mode = false,
       })
     end,
   },
---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,62 @@
+return {
+  "nvimdev/dashboard-nvim",
+  event = "VimEnter",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("dashboard").setup({
+      theme = "doom",
+      config = {
+        header = {
+          "",
+          "",
+          "",
+          "  ██▒   █▓ ██▓ ███▄ ▄███▓",
+          "▓██░   █▒▓██▒▓██▒▀█▀ ██▒",
+          " ▓██  █▒░▒██▒▓██    ▓██░",
+          "  ▒██ █░░░██░▒██    ▒██ ",
+          "   ▒▀█░  ░██░▒██▒   ░██▒",
+          "   ░ ▐░  ░▓  ░ ▒░   ░  ░",
+          "   ░ ░░   ▒ ░░  ░      ░",
+          "     ░░   ▒ ░░      ░   ",
+          "      ░   ░         ░   ",
+          "     ░                  ",
+          "",
+          "",
+        },
+        center = {
+          {
+            icon = "  ",
+            desc = "Find File           ",
+            key = "f",
+            action = "Telescope find_files",
+          },
+          {
+            icon = "  ",
+            desc = "Recent Files        ",
+            key = "r",
+            action = "Telescope oldfiles",
+          },
+          {
+            icon = "  ",
+            desc = "Find Text           ",
+            key = "g",
+            action = "Telescope live_grep",
+          },
+          {
+            icon = "  ",
+            desc = "Config              ",
+            key = "c",
+            action = "edit ~/.config/nvim/init.lua",
+          },
+          {
+            icon = "  ",
+            desc = "Quit                ",
+            key = "q",
+            action = "quit",
+          },
+        },
+        footer = {},
+      },
+    })
+  end,
+}
