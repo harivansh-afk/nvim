c86c905c587f5a2247d6ba73be2a77fe8b716479
Harivansh Rathi <rathiharivansh@gmail.com>
2026-01-29 14:47:53 -0500

    relative

--- init.lua
+++ init.lua
@@ -21,7 +21,7 @@ vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46_cache/"
 
 -- Essential vim options
 vim.opt.number = true
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
