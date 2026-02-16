e7e0a25ab4dd559977235d452481a9b62560a56a
Harivansh Rathi <rathiharivansh@gmail.com>
2026-02-16 17:59:31 -0500

    load oil very lazy delay loading oil until required

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,7 +1,7 @@
 return {
   "stevearc/oil.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  lazy = false,
+  event = 'VeryLazy',
   keys = {
     { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
   },
