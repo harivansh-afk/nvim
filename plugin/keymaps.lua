3db2b6cdcee674599b1f080756aad6965563577b
Harivansh Rathi <rathiharivansh@gmail.com>
2026-02-20 19:09:06 -0500

    wrap line

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -22,6 +22,8 @@ map("n", "J", "mzJ`z", { desc = "Join lines keeping cursor position" })
 map("x", "x", '"_x', { desc = "Delete char without yanking" })
 map("x", "p", '"_dP', { desc = "Paste without yanking replaced text" })
 map("n", "<Esc>", "<cmd>nohlsearch<cr>", { desc = "Clear search highlight" })
+map("n", "<leader>tw", "<cmd>setlocal wrap!<cr>", { desc = "Toggle word wrap" })
+
 
 -- Terminal
 map("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
