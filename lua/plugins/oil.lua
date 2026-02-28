02aeb7dd2429cfbc38201eadd84197bc7270c7e4
Harivansh Rathi <rathiharivansh@gmail.com>
2026-02-28 00:08:09 -0500

    changes to lualine

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,40 +1,34 @@
 return {
-    'stevearc/oil.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons', 'malewicz1337/oil-git.nvim' },
-    config = function(_, opts)
-        require('oil').setup(opts)
-        require('oil-git').setup({
-            show_ignored_files = false,
-            show_ignored_directories = false,
-            debounce_ms = 300,
-        })
-        vim.api.nvim_create_autocmd('BufEnter', {
-            callback = function()
-                if vim.bo.filetype == '' then
-                    local path = vim.fn.expand('%:p')
-                    if vim.fn.isdirectory(path) == 1 then
-                        vim.cmd('Oil ' .. path)
-                    end
-                end
-            end,
-            group = vim.api.nvim_create_augroup('AOil', { clear = true }),
-        })
+    'barrettruth/canola.nvim',
+    dependencies = {
+        'nvim-tree/nvim-web-devicons',
+        {
+            'barrettruth/oil-git.nvim',
+            branch = 'fix/signcolumn-utf8-and-callback',
+            opts = {
+                show_ignored_files = false,
+                show_ignored_directories = false,
+                debounce_ms = 300,
+            },
+        },
+    },
+    init = function()
+        vim.g.oil = {
+            default_file_explorer = true,
+            columns = { 'icon' },
+            view_options = { show_hidden = true },
+            keymaps = {
+                ['g?'] = 'actions.show_help',
+                ['<CR>'] = 'actions.select',
+                ['<C-v>'] = 'actions.select_vsplit',
+                ['<C-x>'] = 'actions.select_split',
+                ['<C-p>'] = 'actions.preview',
+                ['<C-c>'] = 'actions.close',
+                ['-'] = 'actions.parent',
+                ['g.'] = 'actions.toggle_hidden',
+            },
+        }
         map('n', '-', '<cmd>Oil<cr>')
         map('n', '<leader>e', '<cmd>Oil<cr>')
     end,
-    opts = {
-        default_file_explorer = true,
-        columns = { 'icon' },
-        view_options = { show_hidden = true },
-        keymaps = {
-            ['g?'] = 'actions.show_help',
-            ['<CR>'] = 'actions.select',
-            ['<C-v>'] = 'actions.select_vsplit',
-            ['<C-x>'] = 'actions.select_split',
-            ['<C-p>'] = 'actions.preview',
-            ['<C-c>'] = 'actions.close',
-            ['-'] = 'actions.parent',
-            ['g.'] = 'actions.toggle_hidden',
-        },
-    },
 }
